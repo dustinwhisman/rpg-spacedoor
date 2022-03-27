95a6419c13b85a191abb4639950e6ec3fc10704f
Dustin Whisman <whisman.dustin@gmail.com>
2022-03-26 21:40:28 -0600

    chore: add manifest with icons

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/scss/');
   eleventyConfig.addWatchTarget('./src/js/');
 
+  eleventyConfig.addPassthroughCopy({ 'src/manifest.json': 'manifest.json' });
+  eleventyConfig.addPassthroughCopy({ 'src/splash_icon.png': 'splash_icon.png' });
+  eleventyConfig.addPassthroughCopy({ 'src/maskable_icon.png': 'maskable_icon.png' });
+
   return {
     dir: {
       input: 'src/pages',
