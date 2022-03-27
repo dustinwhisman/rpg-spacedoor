ad7bcb0a7a774c3d1aa9d2bc9d8fc2a81a1611bd
Dustin Whisman <whisman.dustin@gmail.com>
2022-03-26 22:08:13 -0600

    feat: add service-worker with generic strategy

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/manifest.json': 'manifest.json' });
   eleventyConfig.addPassthroughCopy({ 'src/splash_icon.png': 'splash_icon.png' });
   eleventyConfig.addPassthroughCopy({ 'src/maskable_icon.png': 'maskable_icon.png' });
+  eleventyConfig.addPassthroughCopy({ 'src/service-worker.js': 'service-worker.js' });
 
   return {
     dir: {
